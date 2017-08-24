5023313d3390e4b1ed643d2fd72e69122037a24a
cauldyclark15 <jcing.jc@gmail.com>
2017-08-24 18:27:49 +0800

    For deploy

--- pages/post.js
+++ pages/post.js
@@ -1,11 +1,40 @@
 import Layout from '../components/MyLayout.js'
 import fetch from 'isomorphic-unfetch';
+import Markdown from 'react-markdown';
 
 const Post = (props) => (
     <Layout>
        <h1>{props.show.name}</h1>
        <p>{props.show.summary.replace(/<[/]?p>/g, '')}</p>
        <img src={props.show.image.medium} alt={props.show.name}/>
+       <div className='markdown'>
+         <Markdown source={
+           `Hello Node [link](/link)
+            ### This is a title
+            content
+           `
+         }/>
+       </div>
+       <style jsx global>{`
+          .markdown {
+            font-family: 'Arial'
+          }
+
+          .markdown a {
+            text-decoration: none;
+            color: blue;
+          }
+
+          .markdown a:hover {
+            opacity: 0.6;
+          }
+
+          .markdown h3 {
+            margin: 0;
+            padding: 0;
+            text-transform: uppercase;
+          }
+        `}</style>
     </Layout>
 )
 
